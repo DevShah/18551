20a1decf57a36b11a07309785ee1cc009981a313
gavman <=>
2014-01-31 12:26:15 -0500

    Audio almost working

---
+++ Lab1/Lab1/obj/local/armeabi/objs/ndkaudio/audio.o.d
@@ -0,0 +1,100 @@
+obj/local/armeabi/objs/ndkaudio/audio.o: jni/audio.c \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdio.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/android/api-level.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/types.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdint.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/_types.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/_types.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/posix_types.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/stddef.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/compiler.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/posix_types.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/types.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/types.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/kernel.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdlib.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/string.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/malloc.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/alloca.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/strings.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/memory.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/math.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/limits.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/limits.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/limits.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/internal_types.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/limits.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/syslimits.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/page.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/jni.h \
+ /home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/android/log.h
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdio.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/android/api-level.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/types.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdint.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/_types.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/_types.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/posix_types.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/stddef.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/compiler.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/posix_types.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/types.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/types.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/kernel.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/stdlib.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/string.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/malloc.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/alloca.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/strings.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/memory.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/math.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/limits.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/limits.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/linux/limits.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/internal_types.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/machine/limits.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/sys/syslimits.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/asm/page.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/jni.h:
+
+/home/gavrieladler/Documents/android-ndk-r9c/platforms/android-19/arch-arm/usr/include/android/log.h:
